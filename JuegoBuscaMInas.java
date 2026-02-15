5eaf8a6f7fd8664b45324f27f08131dfb5bb038e
lordAstom <102704944+lordAstom@users.noreply.github.com>
2026-02-15 10:47:55 +0100

    acabo

--- JuegoBuscaMInas.java
+++ JuegoBuscaMInas.java
@@ -4,13 +4,15 @@ import java.util.Random;
 
 public class JuegoBuscaMInas {
 	private Casilla[][] tablero;
-	private int numMinas = 0;
-	private int numMarcadas = 0;
-	
+	private int numMinas = 0,numMarcadas;
+	private boolean exploto, marcoMal;
 	
 	public JuegoBuscaMInas(int dimensiones, int numMinas) {
 		this.numMinas = numMinas;
 		this.iniciarTablero(new Casilla[dimensiones][dimensiones]);
+		this.exploto=false;
+		this.marcoMal = false;
+		this.numMarcadas = 0;
 		
 	}
 
@@ -42,6 +44,7 @@ public class JuegoBuscaMInas {
 		}
 	}
 	private void calcularMinasCercanas() {
+		/* Calcula el número de minas que rodean a cada casilla. */
 		int minas = 0;
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero.length; j++) {
@@ -60,43 +63,102 @@ public class JuegoBuscaMInas {
 							posiciones[k][0]>0 && posiciones[k][1]>0) {
 						if (tablero[posiciones[k][0]][posiciones[k][1]].getTieneMina()) {
 							minas++;
-						}
-						
+						}						
 					}
-
 				}
 				this.tablero[i][j].setNumMinasCercanas(minas);;
-				minas = 0;
-				
-			}
-			
+				minas = 0;				
+			}			
 		}
 	}
 	public void imprimirTablero() {
-		System.out.print(" ");
+		System.out.print("    ");
 		for (int i = 1; i < this.tablero.length+1; i++) {
-			System.out.print("\u001B[32m"+" "+i+"\u001B[0m");		
+			if (i<10) {
+				System.out.print("\u001B[32m"+" 0"+i+"\u001B[0m");
+			}else{
+				System.out.print("\u001B[32m"+" "+i+"\u001B[0m");
+			}					
 		}
 		
 		for (int i = 1; i < this.tablero.length+1; i++) {
 			System.out.println();
-			System.out.print("\u001B[32m"+i+"\u001B[0m");
-			for (int j = 0; j < this.tablero.length; j++) {
-				if (this.tablero[i-1][j].getTieneMina()) {
-					System.out.print(" "+"*");
-				} else if(this.tablero[i-1][j].getEstaOculta()) {
-					System.out.print(" "+this.tablero[i-1][j].getNumMinasCercanas());
-				}else {
-					System.out.print(" "+this.tablero[i-1][j].getNumMinasCercanas());
-				}
+			if (i<10) {
+				System.out.print("\u001B[32m"+" 0"+i+"\u001B[0m");
+			}else{
+				System.out.print("\u001B[32m"+" "+i+"\u001B[0m");
 			}
-			
+			for (int j = 0; j < this.tablero.length; j++) {
+				if (this.tablero[i-1][j].getEstaMarcada()) {
+					System.out.print("  ^");
+				} else if (this.tablero[i-1][j].getTieneMina()) {
+					System.out.print("  *");
+				} else if(this.tablero[i-1][j].getEstaOculta()) {
+					System.out.print("  -");
+				}else {
+					System.out.print("  "+this.tablero[i-1][j].getNumMinasCercanas());
+				}
+			}			
 		}
-		
+		System.out.println();		
+	}
+	
+	public boolean descubrirCasilla(int fila, int columna){
+		/*  Se le pasa por parámetro la fila y columna a descubrir. Devuelve false en
+		caso que ya estuviese descubierta o marcada previamente. También si excede de los límites
+		del tablero.*/
+		if (fila>this.tablero.length || fila< 1 || columna>this.tablero.length || columna<1) {
+			System.out.println("Se han excedido los limites del tablero");
+			return false;
+		}else if (!this.tablero[fila-1][columna-1].getEstaOculta()) {
+			System.out.println("casilla ya Descubierta");
+			return false;
+		}else if (this.tablero[fila-1][columna-1].getEstaMarcada()) {
+			System.out.println("Casilla ya marcada");
+		}
+		if(this.tablero[fila-1][columna-1].getTieneMina()) {
+			this.exploto = true;
+		}
+		this.tablero[fila-1][columna-1].setEstaOculta(false);
+		return true;
 	}
 
-	public int getNumMarcadas() {
-		return numMarcadas;
+	public boolean marcarCasilla(int fila, int columna){
+		/*Se le pasa por parámetro la fila y columna a marcar. Devuelve false en
+		caso que ya estuviese descubierta o marcada previamente. También si excede de los límites
+		del tablero. */ 
+		if (fila>this.tablero.length || fila<1 || columna>this.tablero.length || columna<1) {
+			System.out.println("Se han excedido los limites del tablero");
+			return false;
+		}else if (!this.tablero[fila-1][columna-1].getEstaOculta()) {
+			System.out.println("casilla ya Descubierta");
+			return false;
+		}else if (this.tablero[fila-1][columna-1].getEstaMarcada()) {
+			System.out.println("Casilla ya marcada");
+		}
+		if(!this.tablero[fila-1][columna-1].getTieneMina()) {
+			this.marcoMal = true;
+		}
+		this.tablero[fila-1][columna-1].setEstaMarcada(true);
+		this.numMarcadas++;
+		return true;
 	}
+
+ 	public int causaTerminacionJuego(){
+		/* Devuelve un entero con los siguientes valores: 1 (el jugador ha
+		marcado todas las minas y ha ganado), 2 (el jugador ha perdido porque ha descubierto una
+		casilla con una mina), 3 (el jugador ha perdido porque ha marcado una casilla sin
+		mina), 0 (la partida continúa) */
+		if (this.numMarcadas==this.numMinas) {
+			return 1;
+		}
+		else if (this.exploto) {
+			return 2;
+		}else if (this.marcoMal) {
+			return 3;
+		}
+		return 0;
+	}
+		
 
 }
